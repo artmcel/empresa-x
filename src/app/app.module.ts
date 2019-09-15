3fe27ee7c7200b5df7b0daedc7d5247196710080
arturo <artmcel@hotmail.com>
2019-09-15 15:37:18 -0500

    Agregamos conponente servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
